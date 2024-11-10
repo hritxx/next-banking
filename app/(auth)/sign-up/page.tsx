563e59bf6c0a41821aecd9ee7fe779f0961fed09
hritxx <hriteekroy1869@gmail.com>
2024-11-10 12:24:07 +0530

    implement the next form for auth

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
-const page = () => {
-  return <div>Sign Up</div>;
+const SignUp = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 };
 
-export default page;
+export default SignUp;
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    //mutation /Database / Make fetch request to the server
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    console.log(userData);
+  } catch (error) {
+    console.error(error);
+  }
+};
